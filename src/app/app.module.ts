68b1483d83a18ebd432b15c782c0b776186eb932
Dain <dain.park@my.jcu.edu.au>
2018-12-12 02:15:56 +0800

    Transfer page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { TransferPage } from '../pages/transfer/transfer';
+import { SettingsPage } from '../pages/settings/settings';
+import { TransactionPage } from '../pages/transaction/transaction';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +20,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    TransferPage,
+    SettingsPage,
+    TransactionPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +35,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    TransferPage,
+    SettingsPage,
+    TransactionPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TransferPage } from '../transfer/transfer';
+
 
 @Component({
   selector: 'page-home',
@@ -9,6 +11,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  openTransferPage(){
+    this.navCtrl.push(TransferPage)
   }
 
 }
