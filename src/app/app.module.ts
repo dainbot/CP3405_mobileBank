4295072d43f58c229ba2fa8f899a2395b7423255
Dain <dain.park@my.jcu.edu.au>
2018-12-12 15:52:17 +0800

    Edited transfer page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { TransferPage } from '../pages/transfer/transfer';
 import { SettingsPage } from '../pages/settings/settings';
 import { TransactionPage } from '../pages/transaction/transaction';
+import { ConfirmTransPage } from '../pages/confirm-trans/confirm-trans';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -23,7 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage,
     TransferPage,
     SettingsPage,
-    TransactionPage
+    TransactionPage,
+    ConfirmTransPage
   ],
   imports: [
     BrowserModule,
@@ -38,7 +40,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage,
     TransferPage,
     SettingsPage,
-    TransactionPage
+    TransactionPage,
+    ConfirmTransPage
   ],
   providers: [
     StatusBar,
