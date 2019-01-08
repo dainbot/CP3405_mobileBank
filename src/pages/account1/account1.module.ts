1c2e4b3e7ecd5bc611c19859935afe918986eb61
anniebbcute <44990621+anniebbcute@users.noreply.github.com>
2019-01-08 18:24:15 +0800

    Update account1.module.ts

--- src/pages/account1/account1.module.ts
+++ src/pages/account1/account1.module.ts
@@ -4,9 +4,11 @@ import { Account1Page } from './account1';
 
 @NgModule({
   declarations: [
+      //     declare the component belong to NgModule
     Account1Page,
   ],
   imports: [
+     //     imports the IonicPageModule to NgModule
     IonicPageModule.forChild(Account1Page),
   ],
 })
